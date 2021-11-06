a05eaaf2b7a7ba7b1d469f26ad3fc398f32ec161
Dasoop <ssummer.lance@gmail.com>
2021-11-05 21:14:57 -0700

    save before deleting module

--- jdkSwitchApp/src/main/java/com/dassoop/jdkswitchapp/MainController.java
+++ jdkSwitchApp/src/main/java/com/dassoop/jdkswitchapp/MainController.java
@@ -7,20 +7,30 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.input.Clipboard;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+//import java.awt.*;
 
 public class MainController implements Initializable
 {
     @FXML
     Label lblDirectory;
+    @FXML
+    Label lblVersion;
+
     @FXML
     ListView<String> listView = new ListView<String>();
 
@@ -36,7 +46,26 @@ public class MainController implements Initializable
     @Override
     public void initialize(URL arg0, ResourceBundle arg1)
     {
+        File dir = new File("/Library/Java/JavaVirtualMachines");
+        directory = dir.getAbsolutePath();
+        File[] listOfContents = dir.listFiles();
+
+
+        Arrays.sort(listOfContents);
+        listView.getItems().clear();
+        listView.getItems().addAll(filesService.getFileNames(listOfContents));
+
+        lblDirectory.setText(dir.getAbsolutePath());
         lblDirectory.setText(directory);
+
+        try
+        {
+            lblVersion.setText(this.checkJavaVersion());
+        }
+        catch(IOException | InterruptedException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void chooseDirectory(ActionEvent event)
@@ -47,6 +76,7 @@ public class MainController implements Initializable
         Stage thisStage = (Stage) node.getScene().getWindow();
 
         File dir = dirChooser.showDialog(thisStage);
+        directory = dir.getAbsolutePath();
         File[] listOfContents = dir.listFiles();
 
 
@@ -56,6 +86,55 @@ public class MainController implements Initializable
 
         lblDirectory.setText(dir.getAbsolutePath());
         event.consume();
+    }
+
+    public String checkJavaVersion() throws IOException, InterruptedException
+    {
+        String cmd = "java --version";
+        Runtime run = Runtime.getRuntime();
+        Process pr = run.exec(cmd);
+        pr.waitFor();
+        BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+
+        List<String> lines = new LinkedList();
+
+        String line = "";
+        while ((line=buf.readLine())!=null)
+        {
+            lines.add(line);
+//            System.out.println(line);
+        }
+        String version = lines.get(0);
+        System.out.println(version);
+        lblVersion.setText(version);
+        return version;
+    }
+
+    public void copyExportCommand()
+    {
+        String fileName = listView.getSelectionModel().getSelectedItem();
+        String cmd = "export JAVA_HOME=" + directory + "/" + fileName + "/Contents/Home";
+        System.out.println(cmd);
+
+//        Toolkit toolkit = Toolkit.getDefaultToolkit();
+//        Clipboard clipboard = toolkit.getSystemClipboard();
+//        StringSelection strSel = new StringSelection(str);
+//        clipboard.setContents(strSel, null);
+
+    }
+
+    public void shellCommand() throws IOException, InterruptedException
+    {
+        String cmd = "ls";
+        Runtime run = Runtime.getRuntime();
+        Process pr = run.exec(cmd);
+        pr.waitFor();
+        BufferedReader buf = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+        String line = "";
+        while ((line=buf.readLine())!=null)
+        {
+            System.out.println(line);
+        }
     }
 
 }
